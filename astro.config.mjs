0a10922571414a6bfe141b3f411c4a1be16fc1ea
Meng-Heng <havmengheng111@gmail.com>
2026-04-30 15:13:37 +0700

    feat: rename site with https

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'clo-cambodia.org',
+  site: 'https://clo-cambodia.org',
   base: '/',
   trailingSlash: "always",
 });
